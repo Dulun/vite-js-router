93bc29ff776c0e6a73c8702e9b89b16b2a162bd4
Dulun <dulunix@gmail.com>
2023-08-15 16:33:08 +0800

    feat: 🎸 modal with animate

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+import cx from 'classnames'
+import styles from './index.module.scss'
+
+const Modal = (props) => {
+  const { visible } = props
+  const modalRef = useRef(null)
+  const isAnimating = useRef(false)
+  const modalWrapperRef = useRef(null)
+
+  const [_visible, setVisible] = useState(false)
+
+  const onShow = () => {
+    modalWrapperRef?.current?.classList.add(styles.fadeIn)
+    modalRef?.current?.classList.add(styles.fadeIn)
+    setVisible(true)
+  }
+
+  const onHide = () => {
+    if (isAnimating.current) return
+    isAnimating.current = true
+
+    modalWrapperRef?.current?.classList.add(styles.fadeOut)
+    modalRef?.current?.classList.add(styles.fadeOut)
+
+    const timer = setTimeout(() => {
+      setVisible(false)
+      props.onClose()
+      isAnimating.current = false
+      clearTimeout(timer)
+    }, 300)
+  }
+
+  useEffect(() => {
+    if (visible) {
+      onShow()
+    }
+    if (!visible) {
+      onHide()
+    }
+  }, [visible])
+
+  if (!visible && !_visible) {
+    return null
+  }
+
+  const onModalClicked = (e) => {
+    e.stopPropagation()
+  }
+
+  return createPortal(
+    <div
+      className={styles.modalWrapper}
+      ref={modalWrapperRef}
+      onClick={onHide}
+    >
+      <div
+        ref={modalRef}
+        className={cx(
+          styles.modalContainer,
+          props.className
+        )}
+        style={{
+          left: `calc(50% + ${
+            props?.driftLeftSider / 2
+          }px)`,
+        }}
+        onClick={onModalClicked}
+      >
+        {props.children}
+      </div>
+    </div>,
+    document.body
+  )
+}
+
+export default Modal
--- src/pages/Home/H1/index.jsx
+++ src/pages/Home/H1/index.jsx
@@ -1,11 +1,35 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState, useRef } from 'react'
+import Modal from '@/components/Modal'
+import styles from './index.module.scss'
 const HHome1 = (props) => {
   useEffect(() => {
     console.log('@@@H1', [props])
   }, [props])
 
-  return <div>H1</div>
+  const [modalVisable, setModalVisible] = useState(false)
+
+  const onShowModal = () => {
+    setModalVisible(true)
+  }
+
+  const contaienr = useRef(null)
+
+  return (
+    <div ref={contaienr}>
+      H1
+      <button onClick={onShowModal}>show modal</button>
+      <Modal
+        driftLeftSider={200}
+        visible={modalVisable}
+        onClose={() => {
+          setModalVisible(false)
+          console.log('@@@????')
+        }}
+      >
+        <div className={styles.modal}>13213131231</div>
+      </Modal>
+    </div>
+  )
 }
 
 export default HHome1
